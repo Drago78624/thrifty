8f32c106d9e83abe0fd457c667e1f827416ba0c5
Muhammad Maaz Ahmed <60292456+Drago78624@users.noreply.github.com>
2022-08-15 10:23:40 +0500

    added a funcitoning login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    const url = "http://192.168.0.104:5000/register";
+    const options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    const resp = await fetch(url, options);
+    const r = await resp.json();
+    console.log(r);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      Login
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="First name"
+          {...register("firstName", { required: true, min: 5, maxLength: 80 })}
+        />
+        <input
+          type="text"
+          placeholder="Last name"
+          {...register("lastName", { required: true, min: 5, maxLength: 100 })}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          {...register("username", { required: true, min: 5 })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password", { required: true, max: 64, min: 8 })}
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          {...register("confirmPassword", { required: true })}
+        />
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
